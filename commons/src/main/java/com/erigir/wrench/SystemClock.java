825aef56ff0990fcf395333fe962c76d872c5239
Chris Weiss <bitblit@gmail.com>
2017-09-08 14:55:15 -0700

    Updating some docs

--- commons/src/main/java/com/erigir/wrench/SystemClock.java
+++ commons/src/main/java/com/erigir/wrench/SystemClock.java
@@ -1,53 +1,71 @@
 package com.erigir.wrench;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 /**
  * A simple singleton that allows one to "fake" the system clock.
+ * <p>
  * Once "started", it advances in normal time just like the system clock does,
  * but it can be set to start from any arbitrary time.  Useful for systems that
  * work on a clock schedule but need to be tested against static data.  Only
  * downside is that the consuming system must call SystemClock.nowTime instead of
  * System.currentTimeMillis everywhere in the code.
+ * </p>
  * <p>
+ * There is also an "error offset", set by queries against a time server like NTP, which allows
+ * for fixing issues with the system clock.
+ * </p>
+ * <p>
+ * The "total" offset is these 2 added together, and NOW = System.currentTimeMillis() + total offset
+ * </p>
+ *
  * Created by cweiss on 2/10/16.
  */
 public class SystemClock {
-  //private static final SystemClock INSTANCE = new SystemClock();
-  // This is the actual clock time when the class started
-  // Needed so that we can track time progress
-  private static long FIXED_START_TIME;
-  private static long START_TIME;
+  private static long TESTING_OFFSET = 0L;
+  private static long ERROR_OFFSET = 0L;
 
   private SystemClock() {
     super();
-    FIXED_START_TIME = System.currentTimeMillis();
-    START_TIME = FIXED_START_TIME;
   }
 
   public static void forceStartTime(Date timestamp) {
-    FIXED_START_TIME = System.currentTimeMillis();
-    START_TIME = timestamp.getTime();
+    TESTING_OFFSET = System.currentTimeMillis() - timestamp.getTime();
+  }
+
+  public static void setErrorOffset(long errorOffset) {
+    ERROR_OFFSET = errorOffset;
+  }
+
+  public static long testingOffset() {
+    return TESTING_OFFSET;
+  }
+
+  public static long errorOffset() {
+    return ERROR_OFFSET;
+  }
+
+  public static long totalOffset() {
+    return TESTING_OFFSET + ERROR_OFFSET;
+  }
+
+  public static Long nowErrorOnlyApplied() {
+    return System.currentTimeMillis() + ERROR_OFFSET;
   }
 
   public static Long now() {
-    return START_TIME + delta();
+    return System.currentTimeMillis() + totalOffset();
   }
 
   public static Date nowDate() {
     return new Date(now());
   }
 
-  public static Long delta() {
-    return System.currentTimeMillis() - FIXED_START_TIME;
-  }
-
-  public static void forceStartTimeToSystem() {
-    forceStartTime(new Date());
-  }
-
-  public static Long startTime() {
-    return START_TIME;
+  public static ZonedDateTime zonedDateTime(ZoneId id) {
+    return ZonedDateTime.ofInstant(nowDate().toInstant(), id);
   }
 
 }
+
--- src/site/markdown/index.md
+++ src/site/markdown/index.md
@@ -1,19 +1,31 @@
 # Erigir Wrench General Purpose Tools
 
-General tools for Erigir
+A set of generally useful tools that I use everywhere.  Quick overview:
 
-# Useful Links
+* Commons : Stuff I use everywhere - quiet wrappers, general output streams, simple http manipulation, zip files
+* Ape : Stuff to make it easier to serve APIs from Spring MVC
+* AWS : Stuff for working in Amazon Web Services
+* Drigo : A library for preprocessing directories full of html/js/css to ready it for upload to S3
+* Fluke : Random stuff that is really one-off (like parsing Google Contacts)
+* Shiro-Oauth : A plugin for shiro to use Oauth providers like Google/Facebook
+* Steelpipe : A library to tunnel SSH and then connect to a MariaDB/Mysql instance on the other side
+* Web : Random stuff useful for Webapps served through MVC
+* Zk : Extensions to the ZK library
 
-Some functions that arent in here, and why:
+# Missing Functions
 
-Null safe equals : replace by using Objects.equals (o,o) after in 1.7 and later
-Throwable to string : replace by using commons lang exception utils
-tomemoryformat : replace by FileUtils.byteCountToDisplaySize(long size) in commons io
-date (calculate difference) : use JodaTime or JDK 1.8 and use Days.daysBetween
+Some functions that arent in here (any more!), and why:
+
+* Null safe equals : replace by using Objects.equals (o,o) after in 1.7 and later
+* Throwable to string : replace by using commons lang exception utils
+* toMemoryFormat : replace by FileUtils.byteCountToDisplaySize(long size) in commons io
+* date (calculate difference) : use JodaTime or JDK 1.8 and use Days.daysBetween
 
 # To Release
 
-# Commit all changes
-# Tag the current location with a tag of the form "release-xxx" where xxx is the version number, eg "git tag -a release-1.1.1"
-# Push the branch and tags to master, CircleCI will take it from there, eg "git push origin master --tags"
-# Login to Sonatype and close/release
+Since I'm always forgetting how to do this
+
+* Commit all changes
+* Tag the current location with a tag of the form "release-xxx" where xxx is the version number, eg "git tag -a release-1.1.1"
+* Push the branch and tags to master, CircleCI will take it from there, eg "git push origin master --tags"
+* Login to Sonatype and close/release
