13ba49baea57d27e0f7f9b9490b1018e41622059
Chris Weiss <bitblit@gmail.com>
2017-09-08 20:16:23 -0700

    Another try

--- sendDocsToS3.sh
+++ sendDocsToS3.sh
@@ -1,12 +1,11 @@
-#!/usr/bin/env bash
-echo Uploading docs folders to S3
+echo Uploading docs folders to S3, root is $1
 aws s3 cp target/site $1 --recursive
-aws s3 cp ape/target/site $1/ape --recursive
-aws s3 cp aws/target/site $1/aws --recursive
+#aws s3 cp ape/target/site $1/ape --recursive
+#aws s3 cp aws/target/site $1/aws --recursive
 aws s3 cp commons/target/site $1/commons --recursive
-aws s3 cp drigo/target/site $1/drigo --recursive
-aws s3 cp fluke/target/site $1/fluke --recursive
-aws s3 cp shiro-oauth/target/site $1/shiro-oauth --recursive
-aws s3 cp steelpipe/target/site $1/steelpipe --recursive
-aws s3 cp web/target/site $1/web --recursive
-aws s3 cp zk/target/site $1/zk --recursive
+#aws s3 cp drigo/target/site $1/drigo --recursive
+#aws s3 cp fluke/target/site $1/fluke --recursive
+#aws s3 cp shiro-oauth/target/site $1/shiro-oauth --recursive
+#aws s3 cp steelpipe/target/site $1/steelpipe --recursive
+#aws s3 cp web/target/site $1/web --recursive
+#aws s3 cp zk/target/site $1/zk --recursive
